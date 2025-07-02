3394415474650ded9b3e48098ffbbfb2be8375d9
Sanjana1816 <ranasanjana60@gmail.com>
2025-07-02 18:58:19 +0530

    new folders

--- backend/genai/agent/tool_search.py
+++
@@ -1,4 +0,0 @@
-# from src.tools.tool_generator import generate_tool_definitions, all_tools
-
-# # 1. Generate the tool definitions dynamically
-# tools_json_string = generate_tool_definitions(all_tools)
